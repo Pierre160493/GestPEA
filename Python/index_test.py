a75414fb5f17beab96add6a9f84024688eb5a65e
Pierre GRANGER <pierre160493@gmail.com>
2022-06-09 07:37:35 +0000

    Ajout en-tete pour execution des scripts python

---
+++ Python/index_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Turn on debug mode
+#import cgitb
+#cgitb.enable()
+
+print("Content-Type:text/html;charset=utf-8")
+print()
+print("Hello World TEST: Fichier dans /home/pierre/GestPEA/")
+
+#print "Content-type: text/html"
+#print
+#print "<html><head>"
+#print ""
+#print "</head><body>"
+#print "Hello."
+#print "</body></html>"
