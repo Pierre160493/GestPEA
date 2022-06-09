b9acce98af4ab87afdda2fa3685cec58df8ad3d3
Pierre GRANGER <pierre160493@gmail.com>
2022-06-09 16:00:29 +0000

    Flask App fonctionne

--- Python/main_OK.py
+++ Python/main_OK.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello world ici!"
+    return "Hello world YES 2!"
 if __name__ == "__main__":
     app.run()
