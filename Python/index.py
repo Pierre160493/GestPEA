e1e4be3bb2f74175c0bf496c011b722322e3fc79
Pierre GRANGER <pierre160493@gmail.com>
2022-06-09 14:08:06 +0000

    Oubli commit precedent

--- Python/index.py
+++ Python/index.py
@@ -1,7 +1,17 @@
-def application(environ,start_response):
-    status='200 OK'
-    output='Hello world'
-    response_headers=[('Content-type','text/plain'),
-    ('Content-Length',str(len(output)))]
-    start_response(status,response_headers)
-    return [output]
+#!/usr/bin/env python
+
+# Turn on debug mode
+#import cgitb
+#cgitb.enable()
+
+print("Content-Type:text/html;charset=utf-8")
+print()
+print("Hello World TEST: Fichier dans /home/pierre/GestPEA/")
+
+#print "Content-type: text/html"
+#print
+#print "<html><head>"
+#print ""
+#print "</head><body>"
+#print "Hello."
+#print "</body></html>"
