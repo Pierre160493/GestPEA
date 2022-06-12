ef51210cc8cee703bf08f35c0f46a622ed9541da
Pierre GRANGER <pierre160493@gmail.com>
2022-06-12 10:42:37 +0000

    Petites corrections avant potentiel conflit

---
+++ Python/FlaskApiSimple_11_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#### Code pour que le serveur (apache2) puisse executer les scripts en python
+from flask import Flask
+from flask_restful import Api, Resource
+import random
+
+app = Flask(__name__)
+api = Api(app)
+
+ai_quotes = [
+    {
+        "id": 0,
+        "author": "Kevin Kelly",
+        "quote": "The business plans of the next 10,000 startups are easy to forecast: " +
+                 "Take X and add AI." 
+    }
+]
+
+class Quote(Resource):
+    def get(self, id=0):
+        if id == 0:
+            return random.choice(ai_quotes), 200
+        for quote in ai_quotes:
+            if(quote["id"] == id):
+                return quote, 200
+        return "Quote not found", 404
+
+api.add_resource(Quote, "/", "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
+if __name__ == '__main__':
+    app.run(debug=True)
--- Python/main.wsgi
+++ Python/main.wsgi
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+#### Commentaire necessaire pour dire que l'environnement est en python (sinon Apache2 est paumé)
 
+#### Fichier qui permet de faire le lien entre le serveur Apache2 et les scripts en python
 import logging
 import sys
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, '/home/pierre/GestPEA/Python/')
 from main import app as application
+#from FlaskApi_11 import app as application
+#from FlaskApiSimple_11_test import app as application
 application.secret_key = 'gestPEA'
